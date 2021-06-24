150edf37d105457b2bda24c17d31864dcc26e687
aleksandar <aca.djordjevic1113@gmail.com>
2021-06-24 16:13:11 -0700

    added colorsReducer

--- frontend/src/store/reducers/colorsReducer.js
+++ frontend/src/store/reducers/colorsReducer.js
@@ -1,2 +1,15 @@
 import merge from 'lodash/merge';
 export const LOAD_COLORS = 'LOAD_COLORS';
+
+export default function reducer(state = {}, action) {
+    switch(action.type) {
+
+        case LOAD_COLORS: {
+            return merge({}, state, action.colors);
+        }
+
+        default: 
+            return state;
+    };
+};
+
