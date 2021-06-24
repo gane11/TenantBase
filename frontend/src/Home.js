7654d60ecd7347b63265f393019ae4df5d8bae96
aleksandar <aca.djordjevic1113@gmail.com>
2021-06-24 16:40:58 -0700

    inserted useEffect to get all the colors and layed out most of the HTML for the Home.js component

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,10 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import ColorCard from './ColorCard';
+import {getAllColors} from './store/actions/colorsAction';
 
-const Home = () => {
+const Home = (colors, getAllColors) => {
+
+    useEffect(() => {
+        getAllColors()
+    }, [])
 
     return (
+        <div>
+            <div className="updated-time">
 
+            </div>
+            <div className="title">
+
+            </div>
+            <div className="colors-container">
+                {colors.map(color => {
+                    return(
+                        <ColorCard color={color}/>
+                    )
+                })}
+            </div>
+        </div>
     );
 };
 
