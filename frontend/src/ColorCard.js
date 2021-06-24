846ace369393f67e78d2b333a3e3958407eb0307
aleksandar <aca.djordjevic1113@gmail.com>
2021-06-24 16:46:35 -0700

    implemented most of the HTML for the colorCard component

--- frontend/src/ColorCard.js
+++ frontend/src/ColorCard.js
@@ -1,8 +1,46 @@
 import React from 'react';
 
 
-const ColorCard = () => {
+const ColorCard = (color) => {
+    
+    
+    return (
+        <div className="color-container">
+            <div className="left-side">
+                <div className="color-name">
 
+                </div>
+                <div className="color-createdby">
+
+                </div>
+                <div className="color-rating">
+                    <div className="color-views">
+
+                    </div>
+                    <div className="color-vote">
+
+                    </div>
+                </div>
+            </div>
+            <div className="right-side">
+                <div className="color-color">
+
+                </div>
+                <div className="color-color">
+
+                </div>
+                <div className="color-color">
+
+                </div>
+                <div className="color-color">
+
+                </div>
+                <div className="color-color">
+
+                </div>
+            </div>
+        </div>
+    )
 
 };
 
