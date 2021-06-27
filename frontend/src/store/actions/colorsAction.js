1fd0a626d21338fd84a2546a475c5185ef5640db
aleksandar <aca.djordjevic1113@gmail.com>
2021-06-26 18:22:48 -0700

    created tempColors for testing untill the fetching works

--- frontend/src/store/actions/colorsAction.js
+++ frontend/src/store/actions/colorsAction.js
@@ -1,23 +1,36 @@
 import {LOAD_COLORS} from '../reducers/colorsReducer';
+import fetchJsonp from 'fetch-jsonp';
+
 
 export const load = (colors) => ({type: LOAD_COLORS, colors});
 
 export const getAllColors = () => async (dispatch) => {
+    // fetchJsonp("http://www.colourlovers.com/api/palettes/new?format=json")
+    //   .then(function (response) {
+    //     return response.json();
+    //   })
+    //   .then(function (json) {
+    //     console.log("parsed json", json);
+    //   })
+    //   .catch(function (ex) {
+    //     console.log("parsing failed", ex);
+    //   });
+    // }
     try {
-       const res = await fetch(
-         "http://www.colourlovers.com/api/palettes/new?format=json", 
-         {
-             mode: 'no-cors'
-         } 
-       );
-       if (res.ok) {
-           console.log('HIII')
-         const places = await res.json();
-         if (places) {
-           dispatch(load(places));
+
+        
+         const res = await fetch("http://www.colourlovers.com/api/colors/new");
+
+            console.log(res);
+         if (res.ok) {
+           const colors= await res.json();
+           console.log(colors)
+           if (colors) {
+             dispatch(load(colors));
+           }
          }
-       }
     } catch(e) {
+        console.log('ALEX')
         console.log(e);
     };
 };
